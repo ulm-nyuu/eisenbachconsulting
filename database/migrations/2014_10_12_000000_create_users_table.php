09a56788748aa6a9b86d343f08c18934c2ce5626
Nyuu <ulm.nyuu@gmail.com>
2020-07-05 14:55:27 +0800

    initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('email', 100);
+            $table->string('password', 255);
+            $table->tinyInteger('status');
+            $table->timestamp('date_created', 0)->useCurrent();
+            $table->timestamp('date_updated', 0)->default(DB::raw('NULL ON UPDATE CURRENT_TIMESTAMP'))->nullable();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
